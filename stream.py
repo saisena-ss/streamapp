177d53d53aeeca7631cef13227f4fe448daf581e
saisena1998 <saisena314@gmail.com>
2024-03-09 01:58:19 -0500

    csv

--- stream.py
+++ stream.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 import os
 
-@st.cache
+@st.cache_resource
 def load_data():
     folder = os.getcwd()
     # Load your Excel file here
-    data = pd.read_excel(folder+'/sample.xlsx')
+    data = pd.read_csv(folder+'/samp1.csv')
     return data
 
 # Load the data only once during deployment
