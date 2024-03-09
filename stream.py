269be801c04f3c5a95e504119481498ef49e41b7
saisena1998 <saisena314@gmail.com>
2024-03-09 01:43:37 -0500

    cache

--- stream.py
+++ stream.py
@@ -2,7 +2,15 @@ import streamlit as st
 import pandas as pd
 import os
 
-folder = os.getcwd()
-print(folder)
-df = pd.read_excel(folder+'/2024 PROJECTIONS.xlsx')
-st.dataframe(df)
+@st.cache
+def load_data():
+    folder = os.getcwd()
+    # Load your Excel file here
+    data = pd.read_excel(folder+'/2024 PROJECTIONS.xlsx')
+    return data
+
+# Load the data only once during deployment
+data = load_data()
+
+# Use the loaded data in your app
+st.write(data)
